d1bf9c8dd86eceac6a947fbf3f8b747e1470d7ee
Julie Hu <jhu21495@gmail.com>
2020-05-04 19:59:23 +0000

    wordcount as a function

--- dicts-word-count/wordcount.py
+++ dicts-word-count/wordcount.py
@@ -1,15 +1,18 @@
+import sys
 
+def count_words():
 
-count_file = open('./twain.txt')
+	count_file = open(sys.argv[1])
+	word_counts = {}
 
-word_counts = {}
+	for line in count_file:
+		line = line.rstrip()
+		words = line.split()
 
-for line in count_file:
-	line = line.rstrip()
-	words = line.split()
+		for word in words:
+			word_counts[word]=word_counts.get(word,0) +1 
 
-	for word in words:
-		word_counts[word]=word_counts.get(word,0) +1 
+	for word,value in word_counts.items():
+		print (word, value)
 
-for word,count in word_counts.items():
-	print (word, count)
+count_words()
