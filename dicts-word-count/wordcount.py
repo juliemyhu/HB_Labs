9438d8753cf3838f20e026beb1caf0a145ef487d
Julie Hu <jhu21495@gmail.com>
2020-05-05 00:15:17 +0000

    fixes punctuation and capital words

--- dicts-word-count/wordcount.py
+++ dicts-word-count/wordcount.py
@@ -6,11 +6,16 @@ def count_words():
 	word_counts = {}
 
 	for line in count_file:
-		line = line.rstrip()
+
+		line = line.strip()
+		
 		words = line.split()
 
 		for word in words:
+			word = word.strip(",.!?$%")
+			word = word.lower()
 			word_counts[word]=word_counts.get(word,0) +1 
+
 
 	for word,value in word_counts.items():
 		print (word, value)
