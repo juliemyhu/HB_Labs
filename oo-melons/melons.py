da1ba1fd0bee198f08dbf0a94baf21cf6c3cf150
Julie Hu <jhu21495@gmail.com>
2020-05-08 19:25:51 +0000

    cleaned up Domestic and Internation Melon order fx

--- oo-melons/melons.py
+++ oo-melons/melons.py
@@ -2,9 +2,18 @@
 
 class AbstractMelonOrder():
 
+    def __init__(self, species, qty, order_type, tax ):
+        """Initialize melon order attributes."""
+
+        self.species = species
+        self.qty = qty
+        self.order_type = order_type 
+        self.tax = tax # tax changes 
+        self.shipped = False
+
     def get_total(self):
         """Calculate price, including tax."""
-
+        # christmas melons = 1.5 times as much as baseprice
         base_price = 5
         total = (1 + self.tax) * self.qty * base_price
 
@@ -22,12 +31,8 @@ class DomesticMelonOrder(AbstractMelonOrder):
     def __init__(self, species, qty):
         """Initialize melon order attributes."""
 
-        self.species = species
-        self.qty = qty
-        self.shipped = False
-        self.order_type = "domestic"
-        self.tax = 0.08
-
+        #ordertype and tax is overwritten now
+        super()__init__(species,qty,"domestic",0.08)
 
 
 class InternationalMelonOrder(AbstractMelonOrder):
@@ -35,15 +40,9 @@ class InternationalMelonOrder(AbstractMelonOrder):
 
     def __init__(self, species, qty, country_code):
         """Initialize melon order attributes."""
+        super()__init__(species,qty, "international", 0.17)
 
-        self.species = species
-        self.qty = qty
-        self.country_code = country_code
-        self.shipped = False
-        self.order_type = "international"
-        self.tax = 0.17
-
-
+    # own method for InternationalMelonOrder
     def get_country_code(self):
         """Return the country code."""
 
